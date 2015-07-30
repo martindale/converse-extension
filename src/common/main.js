3b4f1883fb0b4b50722641b730cdd3a1766a60ae
Eric Martindale <eric@ericmartindale.com>
2015-07-30 14:01:13 -0700

    Add working notifications and links!

--- src/common/main.js
+++ src/common/main.js
@@ -1,4 +1,7 @@
-﻿function Converse() {
+var MAX_NOTIF_LENGTH = 140;﻿
+var BASE_URL = 'converse.ericmartindale.com';
+
+function Converse() {
   var self = this;
   self.unreadCount = 0;
 
@@ -12,16 +15,35 @@
 Converse.prototype = {
   _connect: function() {
     var self = this;
-    self.ws = new WebSocket('wss://converse.ericmartindale.com/notifications');
+    self.ws = new WebSocket('ws://' + BASE_URL + '/notifications');
     self.ws.onmessage = function(msg) {
       console.log('message!', msg);
       try {
         var message = JSON.parse(msg.data);
         if (!message.method) return;
         if (!message.params) return;
+        if (!message.params.ops || !message.params.ops.length) return;
         if (message.method === 'patch') {
-          if (message.params.channel === '/notifications') {
-            self._setUnreadCount(++self.unreadCount);
+          if (message.params.ops[0].op === 'add') {
+            if (message.params.channel === '/posts') {
+              self._setUnreadCount(++self.unreadCount);
+              var id = message.params.ops[0].value._id;
+              var content = message.params.ops[0].value.name;
+              content += ' ' + message.params.ops[0].value.description;
+              content = (content.length > MAX_NOTIF_LENGTH) ? content.substring(0, 139) + '…' : content;
+              kango.ui.notifications.show('New Post', content, '/icons/icon48.png', function() {
+                kango.browser.tabs.create({url: 'https://' + BASE_URL + '/posts/' + id });
+              });
+            }
+            if (message.params.channel === '/comments') {
+              self._setUnreadCount(++self.unreadCount);
+              var id = message.params.ops[0].value._id;
+              var content = message.params.ops[0].value.content;
+              content = (content.length > MAX_NOTIF_LENGTH) ? content.substring(0, 139) + '…' : content;
+              kango.ui.notifications.show('New Comment',  content, '/icons/icon48.png', function() {
+                kango.browser.tabs.create({url: 'https://' + BASE_URL + '/comments/' + id });
+              });
+            }
           }
         }
       } catch (e) {
@@ -34,7 +56,7 @@ Converse.prototype = {
     };
   },
   _onCommand: function() {
-    kango.browser.tabs.create({url: 'https://converse.ericmartindale.com/'});
+    kango.browser.tabs.create({url: 'https://' + BASE_URL + '/'});
   },
   _setUnreadCount: function(count) {
     var self = this;
